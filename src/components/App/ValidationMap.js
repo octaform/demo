f933037962fef9750f18256918f7563838e4272a
Christian Fortes <chrisgfortes@gmail.com>
2018-11-19 22:06:54 -0200

    Changes in src/ files

--- src/components/App/ValidationMap.js
+++ src/components/App/ValidationMap.js
@@ -1,25 +1,10 @@
 export default {
   firstName: {
-    // value: 'field value', // (Optional)
     rules: {
       required: true,
       minlength: 4,
-      // array: [
-      //   '(Array:0)',
-      //   '(Array:1)',
-      //   '(Array:2)',
-      // ],
-      // object: {
-      //   key1: '(Key:0)',
-      //   key2: {
-      //     key3: [{
-      //       key4: '(key:4)',
-      //     }],
-      //   },
-      // },
     },
     messages: {
-      // (Optional, if not set will be use default message from validation)
       required: 'First Name is required',
     },
   },
