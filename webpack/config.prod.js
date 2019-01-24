769814c52bfc121628f29e7627738aa255fbca87
Christian Fortes <christian.fortes@iwsbrazil.com>
2019-01-24 14:26:46 -0200

    Extract comments from vendors

--- webpack/config.prod.js
+++ webpack/config.prod.js
@@ -1,10 +1,15 @@
 require('webpack');
 const merge = require('webpack-merge');
 const configBase = require('./config.base');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = merge(configBase, {
   mode: "production",
   optimization: {
-    minimize: true
+    minimizer: [
+      new UglifyJsPlugin({
+        extractComments: true
+      })
+    ]
   }
 });
