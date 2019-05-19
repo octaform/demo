acc73242666aceb7682e125d3a6bb0937fdfa392
Christian Fortes <christian.fortes@iwsbrazil.com>
2019-05-18 23:16:37 -0300

    :: Build updates

--- .circleci/ssh_github-host-fix.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# https://github.com/IntelAI/mlt/issues/466
-mkdir -p ~/.ssh/
-echo -e "Host github.com\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile /dev/null\n" > ~/.ssh/config
-chmod 600 ~/.ssh/config
-ssh-keyscan -Ht rsa github.com >> ~/.ssh/known_hosts
